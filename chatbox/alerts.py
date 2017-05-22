7086d7048f892590bc49ec5a35155feb9913b9ba
Aurélien Valade <aurelien@Aureliens-MacBook-Pro.local>
2017-05-22 13:22:29 +0800

    Minor naming correction

--- chatbox/alerts.py
+++ chatbox/alerts.py
@@ -7,6 +7,6 @@ def check_bad_words(message_pk, message_content):
     message = Message.objects.get(pk=message_pk)
     message_is_valid = True
 
-    if any(word in message_content for word in bad_words):
+    if any(word in message_content for word in BAD_WORDS):
         message.alert = True
         message.save(update_fields=["alert"])
